be9a541dad95f4faf6097f5fdc8979b2cd89f3cf
Wangwange <ltk6031@naver.com>
2020-02-26 17:08:25 +0900

    object inheritance

--- object inheritance.js
+++ object inheritance.js
@@ -3,3 +3,7 @@ var Obj_s = {value_s: 's'};
 
 Obj_s.__proto__ = Obj_S;
 console.log(Obj_s.value_s, Obj_s.value_S);
+
+var Obj2_S = {value_S: 'S'};
+var Obj2_s = Object.create(Obj_S);
+console.log(Obj_s.value_S);
